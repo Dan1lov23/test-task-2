c0b1d6ab583dcb507db4c5ebe6e8b007a782e4bd
Svatoslav Danilov <svy1dan@gmail.com>
2025-10-29 21:28:23 +0300

    Update FavoriteIcon.tsx

--- src/UI components/favorite icon/FavoriteIcon.tsx
+++ src/UI components/favorite icon/FavoriteIcon.tsx
@@ -5,8 +5,6 @@ import {faHeart} from "@fortawesome/free-solid-svg-icons";
 
 import {useStore} from "../../store/store/store.ts";
 
-import {useStore} from "../../store/store/store.ts";
-
 export default function FavoriteIcon({product}:{product:Product}) {
 
     const {toggleFavorite} = useStore();
